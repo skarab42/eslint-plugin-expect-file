deb4dcbe6c0ce198185f6a88c2be35b7523aee07
skarab42 <skarab@bluewin.ch>
2022-11-27 14:46:55 +0100

    test: add rules tests

---
+++ test/name-to-match-extension.test.ts
@@ -0,0 +1,44 @@
+import { RuleTester } from 'eslint';
+import { test } from 'vitest';
+
+import * as rule from '../source/rules/name-to-match-extension.js';
+
+type Tests = {
+  valid?: (string | RuleTester.ValidTestCase)[] | undefined;
+  invalid?: RuleTester.InvalidTestCase[] | undefined;
+};
+
+const ruleTester = new RuleTester();
+const testRule = (tests: Tests) => (): void => ruleTester.run('name-to-match-extension', rule, tests);
+
+test(
+  'name-to-match-extension',
+  testRule({
+    valid: [
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': '.js' }],
+      },
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': ['.js'] }],
+      },
+    ],
+    invalid: [
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': '.ts' }],
+        errors: [{ line: 0, message: "Expected file extension to be '.ts'." }],
+      },
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': ['.ts'] }],
+        errors: [{ line: 0, message: "Expected file extension to be '.ts'." }],
+      },
+    ],
+  }),
+);
---
+++ test/path-to-match-pattern.test.ts
@@ -0,0 +1,44 @@
+import { RuleTester } from 'eslint';
+import { test } from 'vitest';
+
+import * as rule from '../source/rules/path-to-match-pattern.js';
+
+type Tests = {
+  valid?: (string | RuleTester.ValidTestCase)[] | undefined;
+  invalid?: RuleTester.InvalidTestCase[] | undefined;
+};
+
+const ruleTester = new RuleTester();
+const testRule = (tests: Tests) => (): void => ruleTester.run('path-to-match-pattern', rule, tests);
+
+test(
+  'path-to-match-pattern',
+  testRule({
+    valid: [
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': '**/*.js' }],
+      },
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': ['**/*.js'] }],
+      },
+    ],
+    invalid: [
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': '**/*.ts' }],
+        errors: [{ line: 0, message: "Expected file path to match '**/*.ts' pattern." }],
+      },
+      {
+        code: '',
+        filename: 'source/index.js',
+        options: [{ './source/**': ['**/*.ts'] }],
+        errors: [{ line: 0, message: "Expected file path to match '**/*.ts' pattern." }],
+      },
+    ],
+  }),
+);
